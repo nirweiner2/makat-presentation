74800974a73e77aa94d05c267f0eba8eae8b6a0a
Nir <nir.weiner2@gmail.com>
2026-04-09 14:34:25 +0300

    Fix static export: set distDir to out

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "out",
+  basePath: "/makat-presentation",
 };
 
 export default nextConfig;
